8ca11bee98d18de5059525002f90287e0a85c1dc
NaelDev1 <naelmorais314@gmail.com>
2024-05-12 11:41:53 -0300

    Criando Exceptions Filter

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace MyRecipeBook.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if(context.Exception is MyRecipeBookException)
+            HandleProjectException(context);
+        else
+            ThrowUnknowException(context);
+    
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if(context.Exception is ErrorOnValidationException erro)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result =  new BadRequestObjectResult( new ResponseErrorJson(erro.ErrorMessages));
+        }
+    }
+
+    private void ThrowUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessageExceptions.UNKNOW_ERROR));
+    }
+
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,39 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request) 
+    {
+        // validar a request
+        Validate(request);
+
+        // mapear a request em uma entidade
+
+
+        // criptografia da senha
+
+        // salvar no banco de dados
+
+
+        return new ResponseRegisteredUserJson { Name = request.Name};
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+        var result = validator.Validate(request);
+
+        if(result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(e => e.ErrorMessage);
+
+            throw new ErrorOnValidationException(errorMessages.ToList());
+
+        }
+
+    }
+}
