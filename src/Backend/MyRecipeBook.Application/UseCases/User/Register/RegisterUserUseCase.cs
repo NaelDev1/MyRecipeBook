f3c86b0b0d8e5ff4c5cd540db2db7517f54c47b6
NaelDev1 <naelmorais314@gmail.com>
2024-05-13 00:10:01 -0300

    -Creating Dependency injection-- -Creating DbContext--

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,22 +1,40 @@
-﻿using MyRecipeBook.Communication.Requests;
+﻿using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
 public class RegisterUserUseCase
 {
-    public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request) 
+    private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+
+
+
+    public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request) 
     {
         // validar a request
         Validate(request);
 
         // mapear a request em uma entidade
+        var autoMapper = new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapping());
+        }).CreateMapper();
 
+        var user = autoMapper.Map<Domain.Entities.User>(request);
 
         // criptografia da senha
+        var criptografia = new PasswordEncripter();
+        user.Password = criptografia.Encrypt(request.Password);
+      
 
+      
         // salvar no banco de dados
+       
 
 
         return new ResponseRegisteredUserJson { Name = request.Name};
